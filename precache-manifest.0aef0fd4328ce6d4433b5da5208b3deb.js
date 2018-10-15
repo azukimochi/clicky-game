1dabc6584b2b192fcb51992cbe3135df70f7801f
Durian <karen.kua7@gmail.com>
2018-10-15 19:29:22 -0400

    Updates

---
+++ precache-manifest.0aef0fd4328ce6d4433b5da5208b3deb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "26c919deca6a766e3826",
+    "url": "/static/js/main.26c919de.chunk.js"
+  },
+  {
+    "revision": "709840df12e4494d4644",
+    "url": "/static/js/1.709840df.chunk.js"
+  },
+  {
+    "revision": "26c919deca6a766e3826",
+    "url": "/static/css/main.7892e647.chunk.css"
+  },
+  {
+    "revision": "3970e35bc11a0e29dbba8bf9e7fceb96",
+    "url": "/index.html"
+  }
+];
