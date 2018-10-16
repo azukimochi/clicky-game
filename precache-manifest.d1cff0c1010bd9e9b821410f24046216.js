e828946938958145f8a4a5c83aebed7ee349da97
Durian <karen.kua7@gmail.com>
2018-10-16 01:28:36 -0400

    Updates

--- precache-manifest.d1cff0c1010bd9e9b821410f24046216.js
+++ precache-manifest.d1cff0c1010bd9e9b821410f24046216.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
   },
   {
-    "revision": "26c919deca6a766e3826",
-    "url": "/clicky-game/static/js/main.26c919de.chunk.js"
+    "revision": "6d79de8a68cd4d95a246",
+    "url": "/clicky-game/static/js/main.6d79de8a.chunk.js"
   },
   {
     "revision": "709840df12e4494d4644",
     "url": "/clicky-game/static/js/1.709840df.chunk.js"
   },
   {
-    "revision": "26c919deca6a766e3826",
-    "url": "/clicky-game/static/css/main.7892e647.chunk.css"
+    "revision": "6d79de8a68cd4d95a246",
+    "url": "/clicky-game/static/css/main.b9d090c1.chunk.css"
   },
   {
-    "revision": "94e5f45d3065fa362ed21c0de3d5d067",
+    "revision": "5dba8a289d72d4f7c78dafc43a04a3cf",
     "url": "/clicky-game/index.html"
   }
 ];
