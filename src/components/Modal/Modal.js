f8fb0a58506f7dfff4d1d95de59cbff5fad8e9aa
Durian <karen.kua7@gmail.com>
2018-11-25 18:31:33 -0500

    Started styling modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -3,37 +3,39 @@ import "./Modal.css";
 import Modal from 'react-modal';
 
 const customStyles = {
-    content : {
-      top                   : '50%',
-      left                  : '50%',
-      right                 : 'auto',
-      bottom                : 'auto',
-      marginRight           : '-50%',
-      transform             : 'translate(-50%, -50%)'
-    }
-  };
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
+  }
+};
 
 const modal = props => (
 
-      <Modal
-      isOpen={props.modalIsOpen}
-      onRequestClose={props.closeModal}
-      style={customStyles}
-      contentLabel="Example Modal"
-      >
-      
-      <h2>Hello</h2>
-      <button onClick={props.closeModal}>close</button>
-      <div>I am a modal</div>
-      <form>
-        <input />
-        <button>tab navigation</button>
-        <button>stays</button>
-        <button>inside</button>
-        <button>the modal</button>
-      </form>
-      </Modal>
+  <Modal
+    isOpen={props.modalIsOpen}
+    onRequestClose={props.closeModal}
+    style={customStyles}
+    contentLabel="Example Modal"
+  >
+    <div className="modalContainer">
+    <p id="close" onClick={props.closeModal}>x</p>
+    <br/>
+    <h2>How to Play</h2>
+    <div>Remember your childhood cartoons?</div>
+    <div>This is a fun matching game themed after them!</div>
+    <br/>
+    <ul>
+      <li>Click on unique characters to gain a point.</li>
+      <li>If you click on a character that was previously selected, your score will drop to 0 and the game will end.</li>
+      <li>The top score will be recorded.</li>
+    </ul>
+    </div>
+  </Modal>
 
-      )
+)
 
 export default modal;
